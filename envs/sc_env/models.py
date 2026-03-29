c2c316fa02bf73bcab65a529b7a062a3548bb60c
Khushhal <Khushhalsinha0909@gmail.com>
2026-03-30 03:31:42 +0530

    Changes added

--- envs/sc_env/models.py
+++ envs/sc_env/models.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
 from typing import Dict, Any
+from openenv.core.env_server import Action, Observation
 
-class SCAct(BaseModel):
+class SCAct(Action):
     t_id: int
     p_qty: int
 
-class SCObs(BaseModel):
+class SCObs(Observation):
     t_cap: int
     p_ord: int
     reward: float = 0.0
